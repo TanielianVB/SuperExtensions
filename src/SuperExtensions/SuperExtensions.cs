81034796b3dd6869a35e2cc702c71ba3fd2affb1
Tanielian V. Barreira <tanielianvb@gmail.com>
2015-10-02 16:09:12 -0300

    Solution creation.

---
+++ src/SuperExtensions/SuperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperExtensions
+{
+    public static class SuperExtensions
+    {
+        public static object GetDefaultValue(this Type type)
+        {
+            if (type.IsValueType) return Activator.CreateInstance(type);
+            return null;
+        }
+    }
+}
