35c8447c2fc74343bd4e5a3046272b1d8d6543dc
Tanielian V. Barreira <tanielianvb@gmail.com>
2015-10-07 20:05:26 -0300

    Added the T.IsIn

---
+++ src/SuperExtensions/GenericType.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace SuperExtensions
+{
+    public static class GenericTypeExtensions
+    {
+        public static bool IsIn<T>(this T item, params T[] items) => items.Contains(item);
+    }
+}
---
+++ src/SuperExtensions/Type.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SuperExtensions
+{
+    public static class TypeExtensions
+    {
+        public static object GetDefaultValue(this Type type) => type.IsValueType ? Activator.CreateInstance(type) : null;
+    }
+}
