58fb42e1aa6f1e2c27a427cc4ff6bebc3b75b24c
Tanielian V. Barreira <tanielianvb@gmail.com>
2015-10-21 20:49:10 -0300

    Added LambdaExpressionExtensions.

---
+++ src/SuperExtensions/LambdaExpression.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace SuperExtensions
+{
+    public static class LambdaExpressionExtensions
+    {
+        public static PropertyInfo GetProperty(this LambdaExpression expression) => (expression.Body as MemberExpression).Member as PropertyInfo;
+        public static MethodInfo GetMethod(this LambdaExpression expression) => (expression.Body as MethodCallExpression).Method;
+    }
+}
