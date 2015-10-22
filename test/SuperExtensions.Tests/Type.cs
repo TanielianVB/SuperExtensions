eb1f55f40f84c8847d823a3bbd7419d0e1464133
Tanielian V. Barreira <tanielianvb@gmail.com>
2015-10-22 07:30:49 -0300

    Added Xunit Test project.

---
+++ test/SuperExtensions.Tests/Type.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace SuperExtensions.Tests
+{
+    public class TypeExtensionsTest
+    {
+        [Fact]
+        public void GetDefaultValueTest()
+        {
+            var typeInt = typeof(int);
+            Assert.Equal(typeInt.GetDefaultValue(), 0);
+
+            var typeString = typeof(string);
+            Assert.Equal(typeString.GetDefaultValue(), null);
+        }
+    }
+}
