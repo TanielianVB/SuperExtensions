94e0ec3525874981f8de003dd09001587d796b32
Tanielian V. Barreira <tanielianvb@gmail.com>
2015-11-20 16:46:46 -0300

    Added .ToDictionary to IGrouping.

--- src/SuperExtensions/GenericIEnumerable.cs
+++ src/SuperExtensions/GenericIEnumerable.cs
@@ -8,5 +8,6 @@ namespace SuperExtensions
         public static bool IsNullOrEmpty<T>(this IEnumerable<T> source) => source == null || !source.Any();
         public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> items) => items ?? Enumerable.Empty<T>();
         public static string Join<T>(this IEnumerable<T> values, string separator) => string.Join(separator, values);
+        public static Dictionary<TKey, List<TElement>> ToDictionary<TKey, TElement>(this IEnumerable<IGrouping<TKey, TElement>> source) => source.ToDictionary(group => group.Key, group => group.ToList());
     }
 }
