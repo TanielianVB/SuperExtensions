730c357bb3dac1ee84c0c8e5dadc4e7ca8fc7c2d
Tanielian V. Barreira <tanielianvb@gmail.com>
2015-12-11 09:40:20 -0300

    Added .ToJson and .FromJson. #9

--- src/SuperExtensions.Json/GenericType.cs
+++ src/SuperExtensions.Json/GenericType.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace SuperExtensions
 {
     public static class GenericTypeExtensions
     {
+        /// <summary>
+        /// Serializes the specified object and writes the Json document to a string.
+        /// </summary>
+        /// <typeparam name="T">The type of the object that will be serialized.</typeparam>
+        /// <param name="obj">Object that will be serialized.</param>
+        /// <returns>Json representation of the object contained in a string.</returns>
+        public static string ToJson<T>(this T obj) where T : class, new()
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
     }
 }
--- src/SuperExtensions.Json/String.cs
+++ src/SuperExtensions.Json/String.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace SuperExtensions
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// Deserializes the Json contained in the string.
+        /// </summary>
+        /// <typeparam name="T">The type of the object that this json string represents.</typeparam>
+        /// <param name="json">Json string.</param>
+        /// <returns>Object contained in the Json string.</returns>
+        public static T FromJson<T>(this string json) where T : class, new()
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
     }
 }
