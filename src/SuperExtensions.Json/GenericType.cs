3aee1a946020416df40d775174d16447cb9ac8e8
Tanielian V. Barreira <tanielianvb@gmail.com>
2015-12-14 14:10:41 -0300

    Added more .ToJson methods. #9

--- src/SuperExtensions.Json/GenericType.cs
+++ src/SuperExtensions.Json/GenericType.cs
@@ -1,18 +1,106 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace SuperExtensions
 {
     public static class GenericTypeExtensions
     {
         /// <summary>
-        /// Serializes the specified object and writes the Json document to a string.
+        /// Serializes the specified object to a JSON string.
         /// </summary>
         /// <typeparam name="T">The type of the object that will be serialized.</typeparam>
-        /// <param name="obj">Object that will be serialized.</param>
-        /// <returns>Json representation of the object contained in a string.</returns>
-        public static string ToJson<T>(this T obj)
-        {
-            return JsonConvert.SerializeObject(obj);
-        }
+        /// <param name="value">The object to serialize.</param>
+        /// <returns>A JSON string representation of the object.</returns>
+        public static string ToJson<T>(this T value) => JsonConvert.SerializeObject(value);
+        /// <summary>
+        /// Serializes the specified object to a JSON string using a collection of Newtonsoft.Json.JsonConverter.
+        /// </summary>
+        /// <typeparam name="T">The type of the object that will be serialized.</typeparam>
+        /// <param name="value">The object to serialize.</param>
+        /// <param name="converters">A collection converters used while serializing.</param>
+        /// <returns>A JSON string representation of the object.</returns>
+        public static string ToJson<T>(this T value, params JsonConverter[] converters) => JsonConvert.SerializeObject(value, converters);
+        /// <summary>
+        /// Serializes the specified object to a JSON string using Newtonsoft.Json.JsonSerializerSettings.
+        /// </summary>
+        /// <typeparam name="T">The type of the object that will be serialized.</typeparam>
+        /// <param name="value">The object to serialize.</param>
+        /// <param name="settings">The Newtonsoft.Json.JsonSerializerSettings used to serialize the object. If this is null, default serialization settings will be used.</param>
+        /// <returns>A JSON string representation of the object.</returns>
+        public static string ToJson<T>(this T value, JsonSerializerSettings settings) => JsonConvert.SerializeObject(value, settings);
+        /// <summary>
+        /// Serializes the specified object to a JSON string using formatting.
+        /// </summary>
+        /// <typeparam name="T">The type of the object that will be serialized.</typeparam>
+        /// <param name="value">The object to serialize.</param>
+        /// <param name="formatting">Indicates how the output is formatted.</param>
+        /// <returns>A JSON string representation of the object.</returns>
+        public static string ToJson<T>(this T value, Formatting formatting) => JsonConvert.SerializeObject(value, formatting);
+        /// <summary>
+        /// Serializes the specified object to a JSON string using formatting and a collection of Newtonsoft.Json.JsonConverter.
+        /// </summary>
+        /// <typeparam name="T">The type of the object that will be serialized.</typeparam>
+        /// <param name="value">The object to serialize.</param>
+        /// <param name="formatting">Indicates how the output is formatted.</param>
+        /// <param name="converters">A collection converters used while serializing.</param>
+        /// <returns>A JSON string representation of the object.</returns>
+        public static string ToJson<T>(this T value, Formatting formatting, params JsonConverter[] converters) => JsonConvert.SerializeObject(value, formatting, converters);
+        /// <summary>
+        /// Serializes the specified object to a JSON string using a type, formatting and Newtonsoft.Json.JsonSerializerSettings.
+        /// </summary>
+        /// <typeparam name="T">The type of the object that will be serialized.</typeparam>
+        /// <param name="value">The object to serialize.</param>
+        /// <param name="settings">The Newtonsoft.Json.JsonSerializerSettings used to serialize the object. If this is null, default serialization settings will be used.</param>
+        /// <param name="type">The type of the value being serialized. This parameter is used when Newtonsoft.Json.TypeNameHandling is Auto to write out the type name if the type of the value does not match. Specifing the type is optional.</param>
+        /// <returns>A JSON string representation of the object.</returns>
+        public static string ToJson<T>(this T value, Type type, JsonSerializerSettings settings) => JsonConvert.SerializeObject(value, type, settings);
+        /// <summary>
+        /// Serializes the specified object to a JSON string using formatting and Newtonsoft.Json.JsonSerializerSettings.
+        /// </summary>
+        /// <typeparam name="T">The type of the object that will be serialized.</typeparam>
+        /// <param name="value">The object to serialize.</param>
+        /// <param name="formatting">Indicates how the output is formatted.</param>
+        /// <param name="settings">The Newtonsoft.Json.JsonSerializerSettings used to serialize the object. If this is null, default serialization settings will be used.</param>
+        /// <returns>A JSON string representation of the object.</returns>
+        public static string ToJson<T>(this T value, Formatting formatting, JsonSerializerSettings settings) => JsonConvert.SerializeObject(value, formatting, settings);
+        /// <summary>
+        /// Serializes the specified object to a JSON string using a type, formatting and Newtonsoft.Json.JsonSerializerSettings.
+        /// </summary>
+        /// <typeparam name="T">The type of the object that will be serialized.</typeparam>
+        /// <param name="value">The object to serialize.</param>
+        /// <param name="formatting">Indicates how the output is formatted.</param>
+        /// <param name="settings">The Newtonsoft.Json.JsonSerializerSettings used to serialize the object. If this is null, default serialization settings will be used.</param>
+        /// <param name="type">The type of the value being serialized. This parameter is used when Newtonsoft.Json.TypeNameHandling is Auto to write out the type name if the type of the value does not match. Specifing the type is optional.</param>
+        /// <returns>A JSON string representation of the object.</returns>
+        public static string ToJson<T>(this T value, Type type, Formatting formatting, JsonSerializerSettings settings) => JsonConvert.SerializeObject(value, type, formatting, settings);
+
+        /// <summary>
+        /// Asynchronously serializes the specified object to a JSON string. Serialization will happen on a new thread.
+        /// </summary>
+        /// <typeparam name="T">The type of the object that will be serialized.</typeparam>
+        /// <param name="value">The object to serialize.</param>
+        /// <returns>A task that represents the asynchronous serialize operation. The value of the TResult parameter contains a JSON string representation of the object.</returns>
+        [Obsolete("ToJsonAsync is obsolete. Use the Task.Factory.StartNew method to serialize JSON asynchronously: Task.Factory.StartNew(() => value.ToJson())")]
+        public static Task<string> ToJsonAsync<T>(this T value) => JsonConvert.SerializeObjectAsync(value);
+        /// <summary>
+        /// Asynchronously serializes the specified object to a JSON string using formatting. Serialization will happen on a new thread.
+        /// </summary>
+        /// <typeparam name="T">The type of the object that will be serialized.</typeparam>
+        /// <param name="value">The object to serialize.</param>
+        /// <param name="formatting">Indicates how the output is formatted.</param>
+        /// <returns>A task that represents the asynchronous serialize operation. The value of the TResult parameter contains a JSON string representation of the object.</returns>
+        [Obsolete("ToJsonAsync is obsolete. Use the Task.Factory.StartNew method to serialize JSON asynchronously: Task.Factory.StartNew(() => value.ToJson(formatting))")]
+        public static Task<string> ToJsonAsync<T>(this T value, Formatting formatting) => JsonConvert.SerializeObjectAsync(value, formatting);
+        /// <summary>
+        /// Asynchronously serializes the specified object to a JSON string using formatting and a collection of Newtonsoft.Json.JsonConverter. Serialization will happen on a new thread.
+        /// </summary>
+        /// <typeparam name="T">The type of the object that will be serialized.</typeparam>
+        /// <param name="value">The object to serialize.</param>
+        /// <param name="formatting">Indicates how the output is formatted.</param>
+        /// <param name="settings">The Newtonsoft.Json.JsonSerializerSettings used to serialize the object. If this is null, default serialization settings will be used.</param>
+        /// <returns>A task that represents the asynchronous serialize operation. The value of the TResult parameter contains a JSON string representation of the object.</returns>
+        [Obsolete("ToJsonAsync is obsolete. Use the Task.Factory.StartNew method to serialize JSON asynchronously: Task.Factory.StartNew(() => value.ToJson(formatting, settings))")]
+        public static Task<string> ToJsonAsync<T>(this T value, Formatting formatting, JsonSerializerSettings settings) => JsonConvert.SerializeObjectAsync(value, formatting, settings);
     }
 }
--- src/SuperExtensions.Json/String.cs
+++ src/SuperExtensions.Json/String.cs
@@ -1,18 +1,102 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace SuperExtensions
 {
     public static class StringExtensions
     {
         /// <summary>
-        /// Deserializes the Json contained in the string.
+        /// Deserializes the JSON to a .NET object.
         /// </summary>
-        /// <typeparam name="T">The type of the object that this json string represents.</typeparam>
-        /// <param name="json">Json string.</param>
-        /// <returns>Object contained in the Json string.</returns>
-        public static T FromJson<T>(this string json)
-        {
-            return JsonConvert.DeserializeObject<T>(json);
-        }
+        /// <param name="value">The JSON to deserialize.</param>
+        /// <returns>The deserialized object from the JSON string.</returns>
+        public static object FromJson(this string value) => JsonConvert.DeserializeObject(value);
+        /// <summary>
+        /// Deserializes the JSON to a .NET object using Newtonsoft.Json.JsonSerializerSettings.
+        /// </summary>
+        /// <param name="value">The JSON to deserialize.</param>
+        /// <param name="settings">The Newtonsoft.Json.JsonSerializerSettings used to deserialize the object. If this is null, default serialization settings will be used.</param>
+        /// <returns>The deserialized object from the JSON string.</returns>
+        public static object FromJson(this string value, JsonSerializerSettings settings) => JsonConvert.DeserializeObject(value, settings);
+        /// <summary>
+        /// Deserializes the JSON to the specified .NET type.
+        /// </summary>
+        /// <param name="value">The JSON to deserialize.</param>
+        /// <param name="type">The System.Type of object being deserialized.</param>
+        /// <returns>The deserialized object from the JSON string.</returns>
+        public static object FromJson(this string value, Type type) => JsonConvert.DeserializeObject(value, type);
+        /// <summary>
+        /// Deserializes the JSON to the specified .NET type using a collection of Newtonsoft.Json.JsonConverter.
+        /// </summary>
+        /// <param name="value">The JSON to deserialize.</param>
+        /// <param name="type">The type of the object to deserialize.</param>
+        /// <param name="converters">Converters to use while deserializing.</param>
+        /// <returns>The deserialized object from the JSON string.</returns>
+        public static object FromJson(this string value, Type type, params JsonConverter[] converters) => JsonConvert.DeserializeObject(value, type, converters);
+        /// <summary>
+        /// Deserializes the JSON to the specified .NET type using Newtonsoft.Json.JsonSerializerSettings.
+        /// </summary>
+        /// <param name="value">The JSON to deserialize.</param>
+        /// <param name="type">The type of the object to deserialize to.</param>
+        /// <param name="settings">The Newtonsoft.Json.JsonSerializerSettings used to deserialize the object. If this is null, default serialization settings will be used.</param>
+        /// <returns>The deserialized object from the JSON string.</returns>
+        public static object FromJson(this string value, Type type, JsonSerializerSettings settings) => JsonConvert.DeserializeObject(value, type, settings);
+        /// <summary>
+        /// Deserializes the JSON to the specified .NET type.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to deserialize to.</typeparam>
+        /// <param name="value">The JSON to deserialize.</param>
+        /// <returns>The deserialized object from the JSON string.</returns>
+        public static T FromJson<T>(this string value) => JsonConvert.DeserializeObject<T>(value);
+        /// <summary>
+        /// Deserializes the JSON to the specified .NET type using Newtonsoft.Json.JsonSerializerSettings.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to deserialize to.</typeparam>
+        /// <param name="value">The object to deserialize.</param>
+        /// <param name="settings">The Newtonsoft.Json.JsonSerializerSettings used to deserialize the object. If this is null, default serialization settings will be used.</param>
+        /// <returns>The deserialized object from the JSON string.</returns>
+        public static T FromJson<T>(this string value, JsonSerializerSettings settings) => JsonConvert.DeserializeObject<T>(value, settings);
+        /// <summary>
+        /// Deserializes the JSON to the specified .NET type using a collection of Newtonsoft.Json.JsonConverter.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to deserialize to.</typeparam>
+        /// <param name="value">The JSON to deserialize.</param>
+        /// <param name="converters">Converters to use while deserializing.</param>
+        /// <returns>The deserialized object from the JSON string.</returns>
+        public static T FromJson<T>(this string value, params JsonConverter[] converters) => JsonConvert.DeserializeObject<T>(value, converters);
+
+        /// <summary>
+        /// Asynchronously deserializes the JSON to the specified .NET type. Deserialization will happen on a new thread.
+        /// </summary>
+        /// <param name="value">The JSON to deserialize.</param>
+        /// <returns>A task that represents the asynchronous deserialize operation. The value of the TResult parameter contains the deserialized object from the JSON string.</returns>
+        [Obsolete("FromJsonAsync is obsolete. Use the Task.Factory.StartNew method to deserialize JSON asynchronously: Task.Factory.StartNew(() => value.FromJsonAsync())")]
+        public static Task<object> FromJsonAsync(this string value) => JsonConvert.DeserializeObjectAsync(value);
+        /// <summary>
+        /// Asynchronously deserializes the JSON to the specified .NET type using Newtonsoft.Json.JsonSerializerSettings. Deserialization will happen on a new thread.
+        /// </summary>
+        /// <param name="value">The JSON to deserialize.</param>
+        /// <param name="type">The type of the object to deserialize to.</param>
+        /// <param name="settings">The Newtonsoft.Json.JsonSerializerSettings used to deserialize the object. If this is null, default serialization settings will be used.</param>
+        /// <returns>A task that represents the asynchronous deserialize operation. The value of the TResult parameter contains the deserialized object from the JSON string.</returns>
+        [Obsolete("FromJsonAsync is obsolete. Use the Task.Factory.StartNew method to deserialize JSON asynchronously: Task.Factory.StartNew(() => value.FromJsonAsync(type, settings))")]
+        public static Task<object> FromJsonAsync(this string value, Type type, JsonSerializerSettings settings) => JsonConvert.DeserializeObjectAsync(value, type, settings);
+        /// <summary>
+        /// Asynchronously deserializes the JSON to the specified .NET type. Deserialization will happen on a new thread.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to deserialize to.</typeparam>
+        /// <param name="value">The JSON to deserialize.</param>
+        /// <returns>A task that represents the asynchronous deserialize operation. The value of the TResult parameter contains the deserialized object from the JSON string.</returns>
+        [Obsolete("FromJsonAsync is obsolete. Use the Task.Factory.StartNew method to deserialize JSON asynchronously: Task.Factory.StartNew(() => value.FromJsonAsync<T>())")]
+        public static Task<T> FromJsonAsync<T>(this string value) => JsonConvert.DeserializeObjectAsync<T>(value);
+        /// <summary>
+        /// Asynchronously deserializes the JSON to the specified .NET type using Newtonsoft.Json.JsonSerializerSettings. Deserialization will happen on a new thread.
+        /// </summary>
+        /// <param name="value">The JSON to deserialize.</param>
+        /// <param name="settings">The Newtonsoft.Json.JsonSerializerSettings used to deserialize the object. If this is null, default serialization settings will be used.</param>
+        /// <returns>A task that represents the asynchronous deserialize operation. The value of the TResult parameter contains the deserialized object from the JSON string.</returns>
+        [Obsolete("FromJsonAsync is obsolete. Use the Task.Factory.StartNew method to deserialize JSON asynchronously: Task.Factory.StartNew(() => value.FromJsonAsync<T>(settings))")]
+        public static Task<T> FromJsonAsync<T>(this string value, JsonSerializerSettings settings) => JsonConvert.DeserializeObjectAsync<T>(value, settings);
     }
 }
